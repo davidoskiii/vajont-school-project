4d3b7e3823c49fd0c780425782679765d954c7e8
_davidoskiii <davide.mascherin9@gmail.com>
2023-11-10 17:06:46 +0100

    Changed the look of the History section, still in progress

--- src/components/History.svelte
+++ src/components/History.svelte
@@ -1,18 +1,62 @@
 <script>
+    import { scrollTo, scrollRef, scrollTop } from 'svelte-scrolling'
     import HorizzontalTimeline from "./Timelines/HorizzontalTimeline.svelte";
-    import VerticalTimeline from "./Timelines/VerticalTimeline.svelte"
+    import Carousel from 'svelte-carousel'
 </script>
 
 <main>
     <div class="history">
         <div class="text-div">
+            <h2>Storia</h2>
+            <div class="history-paragraph-start start-history m-5">
+                <div class="m-5">
+                    <h1>Viaggio nel Passato: La valle del disastro</h1>
+                    <p>Entra nel cuore di una delle pagine più oscure della storia italiana. Scopri la narrazione completa del disastro del Vajont, una tragedia che ha sconvolto il mondo nel 1963. Attraverso una sequenza cronologica dettagliata e testimonianze dirette, questa sezione rivelerà gli eventi che hanno portato a una catastrofe senza precedenti, gettando luce sui protagonisti, le decisioni cruciali e le conseguenze durature che hanno segnato questa epocale tragedia.</p>
+                </div>
+                <div class="carousel-container m-5">
+                    <Carousel swiping={false} autoplay autoplayDuration={2000}>
+                        <img class="image-carousel" src="https://media-assets.wired.it/photos/6523ca3b7ff82f5e7241bda0/16:9/w_1920,c_limit/158986304" alt="elicottero disastro">
+                        <img class="image-carousel" src="https://www.raicultura.it/dl/img/2019/10/04/1570203895060_h_00101051.jpg" alt="elicottero disastro">
+                        <img class="image-carousel" src="https://www.raicultura.it/dl/img/2019/10/04/1570203895060_h_00101050.jpg" alt="elicottero disastro">
+                    </Carousel>
+                </div>
+            </div>
             <HorizzontalTimeline/>
-            <h1>La Tragedia del Vajont: Quando la Montagna Tremò</h1>
-            <p>Nella storia del Vajont, uno dei disastri più devastanti della storia italiana, la natura si è scontrata con l'ingegno umano in una tragedia inimmaginabile. Questo racconto esplora gli eventi che hanno portato al crollo della montagna del Vajont nel 1963, provocando una delle peggiori catastrofi della storia moderna. Dalle cause alla devastazione e alle conseguenze, questa storia è un ricordo doloroso delle vite perdute e delle lezioni apprese dall'errore umano.</p>
+            <div class="history-paragraph-image">
+                <div class="sibling-image m-5">
+                    <img class="image-carousel rounded-image" src="https://media-assets.wired.it/photos/6523ca3b7ff82f5e7241bda0/16:9/w_1920,c_limit/158986304" alt="elicottero disastro">
+                </div>
+                <div class="m-5">
+                    <h1>Viaggio nel Passato: La valle del disastro</h1>
+                    <p>Entra nel cuore di una delle pagine più oscure della storia italiana. Scopri la narrazione completa del disastro del Vajont, una tragedia che ha sconvolto il mondo nel 1963. Attraverso una sequenza cronologica dettagliata e testimonianze dirette, questa sezione rivelerà gli eventi che hanno portato a una catastrofe senza precedenti, gettando luce sui protagonisti, le decisioni cruciali e le conseguenze durature che hanno segnato questa epocale tragedia.</p>
+                </div>
+                <section use:scrollRef={'1990'}></section>
+            </div>
+            <div class="history-paragraph">
+                <h1>La Tragedia del Vajont: Quando la Montagna Tremò</h1>
+                <p>Nella storia del Vajont, uno dei disastri più devastanti della storia italiana, la natura si è scontrata con l'ingegno umano in una tragedia inimmaginabile. Questo racconto esplora gli eventi che hanno portato al crollo della montagna del Vajont nel 1963, provocando una delle peggiori catastrofi della storia moderna. Dalle cause alla devastazione e alle conseguenze, questa storia è un ricordo doloroso delle vite perdute e delle lezioni apprese dall'errore umano.</p>
+            </div>
+            <div class="history-paragraph">
+                <h1>La Tragedia del Vajont: Quando la Montagna Tremò</h1>
+                <p>Nella storia del Vajont, uno dei disastri più devastanti della storia italiana, la natura si è scontrata con l'ingegno umano in una tragedia inimmaginabile. Questo racconto esplora gli eventi che hanno portato al crollo della montagna del Vajont nel 1963, provocando una delle peggiori catastrofi della storia moderna. Dalle cause alla devastazione e alle conseguenze, questa storia è un ricordo doloroso delle vite perdute e delle lezioni apprese dall'errore umano.</p>
+            </div>
+            <div class="history-paragraph centered-flex">
+                <div class="m-5">
+                    <h1>La Tragedia del Vajont: Quando la Montagna Tremò</h1>
+                    <p>Nella storia del Vajont, uno dei disastri più devastanti della storia italiana, la natura si è scontrata con l'ingegno umano in una tragedia inimmaginabile. Questo racconto esplora gli eventi che hanno portato al crollo della montagna del Vajont nel 1963, provocando una delle peggiori catastrofi della storia moderna. Dalle cause alla devastazione e alle conseguenze, questa storia è un ricordo doloroso delle vite perdute e delle lezioni apprese dall'errore umano.</p>
+                </div>
+                <div class="carousel-container m-5">
+                    <Carousel swiping={false} autoplay autoplayDuration={2000}>
+                        <img class="image-carousel" src="https://media-assets.wired.it/photos/6523ca3b7ff82f5e7241bda0/16:9/w_1920,c_limit/158986304" alt="elicottero disastro">
+                        <img class="image-carousel" src="https://www.raicultura.it/dl/img/2019/10/04/1570203895060_h_00101051.jpg" alt="elicottero disastro">
+                        <img class="image-carousel" src="https://www.raicultura.it/dl/img/2019/10/04/1570203895060_h_00101050.jpg" alt="elicottero disastro">
+                    </Carousel>
+                </div>
+            </div>
         </div>
-        <div class="vertical-timeline">
+        <!-- <div class="vertical-timeline">
             <VerticalTimeline/>
-        </div>
+        </div> -->
     </div>
 </main>
 
--- src/components/Timelines/HorizzontalTimeline.svelte
+++ src/components/Timelines/HorizzontalTimeline.svelte
@@ -3,17 +3,19 @@
 </script>
 
 <main>
-    <div class="timeline">
-        <!-- svelte-ignore a11y-missing-attribute -->
-        <a use:scrollTo={'introduzione'} class="dot year-1990">1990</a>
-        <!-- svelte-ignore a11y-missing-attribute -->
-        <a use:scrollTo={'introduzione'} class="dot year-2000">2000</a>
-        <!-- svelte-ignore a11y-missing-attribute -->
-        <a use:scrollTo={'introduzione'} class="dot year-2010">2010</a>
-        <!-- svelte-ignore a11y-missing-attribute -->
-        <a use:scrollTo={'introduzione'} class="dot year-2020">2020</a>
-        <!-- svelte-ignore a11y-missing-attribute -->
-        <a use:scrollTo={'introduzione'} class="dot year-2030">2030</a>
+    <div class="centered-flex">
+        <div class="timeline">
+            <!-- svelte-ignore a11y-missing-attribute -->
+            <a use:scrollTo={'1990'} class="dot year-1990">1990</a>
+            <!-- svelte-ignore a11y-missing-attribute -->
+            <a use:scrollTo={'introduzione'} class="dot year-2000">2000</a>
+            <!-- svelte-ignore a11y-missing-attribute -->
+            <a use:scrollTo={'introduzione'} class="dot year-2010">2010</a>
+            <!-- svelte-ignore a11y-missing-attribute -->
+            <a use:scrollTo={'introduzione'} class="dot year-2020">2020</a>
+            <!-- svelte-ignore a11y-missing-attribute -->
+            <a use:scrollTo={'introduzione'} class="dot year-2030">2030</a>
+        </div>
     </div>
 </main>
 
