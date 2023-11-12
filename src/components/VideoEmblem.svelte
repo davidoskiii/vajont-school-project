b57ad01c63c7a49cb7c6abbfc986907dea55078b
_davidoskiii <davide.mascherin9@gmail.com>
2023-11-12 20:54:12 +0100

    Test commit for gh-pages

--- src/components/VideoEmblem.svelte
+++ src/components/VideoEmblem.svelte
@@ -4,12 +4,13 @@
 
 <main>
     <!-- svelte-ignore a11y-media-has-caption -->
-    <video class="video" src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" controls></video>
+    <iframe class="video" src="https://drive.google.com/file/d/1cKUY5uFmX3n8Tys5AgAWZw7bxMi42Rc0/preview" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
 </main>
 
 <style>
     .video {
         border-radius: 10px;
         width: 600px;
+        height: 337.5px;
     }
 </style>
